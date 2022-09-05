ed1be753d09551fe997392d46baa37b54d8642f9
Sathwik Pothunoori <sathwikpothunoori@gmail.com>
2022-09-06 00:52:07 +0530

    serving static html using express

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 
 const http = require('http');
-
+// morgan will basically log on additinal information on the screen
+const morgan = require('morgan')
 const hostname ='localhost'
 
 const port =3000
 
 const app =express()
 
+app.use(morgan('dev'))
+
+app.use(express.static(__dirname +'/public')) //this will be the page from where static html files will be served
+
+
 app.use((req , res  , next)=>{
-    //  console.log(req.headers)
+    //console.log(req.headers)
      res.statusCode=200
-     res.contentType('Content-Type','tyext/html')
+     res.setHeader('Content-Type','text/html')
      res.end('<html><body><h1>Hello from express server</h1></body></html>')
 })
 
