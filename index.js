c718b0d845c0f9bcc8a97916b15abd961a1dd560
Sathwik Pothunoori <sathwikpothunoori@gmail.com>
2022-09-06 14:51:02 +0530

    changed from library

--- index.js
+++ index.js
@@ -14,15 +14,16 @@ app.use(morgan('dev'))
 app.use(express.static(__dirname +'/public')) //this will be the page from where static html files will be served
 
 
-app.use((req , res  , next)=>{
-    //console.log(req.headers)
-     res.statusCode=200
-     res.setHeader('Content-Type','text/html')
-     res.end('<html><body><h1>Hello from express server</h1></body></html>')
-})
+// app.use((req , res  , next)=>{
+//     //console.log(req.headers)
+//      res.statusCode=200
+//      res.setHeader('Content-Type','text/html')
+//      res.end('<html><body><h1>Hello from express server</h1></body></html>')
+// })
 
 const server = http.createServer(app)
 
 server.listen(port, hostname , ()=>{
     console.log(`Serverf running at http://${hostname}:${port}`)
 })
+
